46d52772c8bcb79a18fe70f2d396da966388737f
aranney17 <aranney.sivakumar@etu.cyu.fr>
2025-12-02 12:54:00 +0100

    Implement lecture function for CSV file reading
    Add function to read data from a CSV file and handle file opening errors.

--- Fichiers C/lesture_CSV.c
+++ Fichiers C/lesture_CSV.c
@@ -1,1 +1,13 @@
 #include "lecture_CSV.h"
+
+void lecture(){
+
+  //Gestion du fichier
+	FILE* fichier=NULL;
+	fichier=fopen("Donnees/c-wildwater_v0(1).dat","r+");
+	if (fichier==NULL ){
+		printf("Ouverture du fichier impossible\n");
+		exit(1);
+	}
+	
+	while (caractereActuel != EOF){
