6135db7b44e20b99ee2728ce539a3758c38701fc
aranney17 <aranney.sivakumar@etu.cyu.fr>
2025-12-02 12:28:15 +0100

    Update avl.h

--- Fichier H/avl.h
+++ Fichier H/avl.h
@@ -1,1 +1,13 @@
+// Bibliothèques
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
+//structure AVL
+typedef struct AVL{
+	int val;
+	struct arbre* fg;
+	struct arbre* fd;
+	int equilibre;
+}AVL;
