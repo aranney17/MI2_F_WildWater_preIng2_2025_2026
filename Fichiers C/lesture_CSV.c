8e29c6e27c3c71be0802d483c761e72ab9b13a0d
aranney17 <aranney.sivakumar@etu.cyu.fr>
2025-12-08 16:16:04 +0100

    Update lesture_CSV.c

--- Fichiers C/lesture_CSV.c
+++ Fichiers C/lesture_CSV.c
@@ -1,13 +1,41 @@
 #include "lecture_CSV.h"
 
+
 void lecture(){
 
-  //Gestion du fichier
+	//Gestion du fichier
 	FILE* fichier=NULL;
-	fichier=fopen("Donnees/c-wildwater_v0(1).dat","r+");
+	fichier=fopen("c-wildwater_v0(1).dat","r+");
 	if (fichier==NULL ){
 		printf("Ouverture du fichier impossible\n");
 		exit(1);
 	}
 	
-	while (caractereActuel != EOF){
+	
+	// variables locales
+	int caractereActuel = 0;
+	char* id[];
+	int compteur=0;
+		
+	
+	// alloue dynamiquement l'espace nécessaire pour stocker l'id de l'usine
+        while(fgetc(fichier)!=';'){ // compte le nb de chiffres dans l'id
+            compteur++;
+        }
+        id=malloc((compteur+1)*sizeof(char));
+        if(id==NULL){
+            printf("Allocation échouée\n");
+            exit(1);
+        }
+        fseek(fichier,-(compteur+1),SEEK_CUR); //curseur replacé devant le premier chiffre de l'id
+        compteur=0;
+        printf ("%s",id);
+	
+	
+}
+
+
+int main(){
+	lecture();
+	return 0;
+}
