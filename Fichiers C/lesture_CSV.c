1fbff89dc2db4fc92fc2771db26685c0485c3719
aranney17 <aranney.sivakumar@etu.cyu.fr>
2025-12-09 13:23:14 +0100

    Update lesture_CSV.c

--- Fichiers C/lesture_CSV.c
+++ Fichiers C/lesture_CSV.c
@@ -1,5 +1,29 @@
 #include "lecture_CSV.h"
 
+	
+	
+void stockage(char* tmp, char caractere,FILE* fichier){
+	int compteur=0;
+		
+	// alloue dynamiquement l'espace nécessaire pour stocker l'id de l'usine
+        while(fgetc(fichier)!=caractere){ // compte le nb de chiffres dans l'id
+            compteur++;
+        }
+        
+        tmp=malloc((compteur+1)*sizeof(char));
+        if(tmp==NULL){
+            printf("Allocation échouée\n");
+            exit(1);
+        }
+        
+        fseek(fichier,-(compteur+1),SEEK_CUR); //curseur replacé devant le premier chiffre de l'id
+        fgets(tmp,compteur+1,fichier);
+        printf("%s\n",tmp);
+	fseek(fichier,1,SEEK_CUR);
+}
+	
+	
+
 
 void lecture(){
 
@@ -13,29 +37,14 @@ void lecture(){
 	
 	
 	// variables locales
-	int caractereActuel = 0;
-	char* id[];
-	int compteur=0;
+	Info ligne;
+	int type=-1;
 		
+	stockage(ligne.col1,';',fichier);
+	stockage(ligne.col2,';',fichier);
+	stockage(ligne.col3,';',fichier);
+	stockage(ligne.volume,';',fichier);
+	stockage(ligne.leak,'\n',fichier);
 	
-	// alloue dynamiquement l'espace nécessaire pour stocker l'id de l'usine
-        while(fgetc(fichier)!=';'){ // compte le nb de chiffres dans l'id
-            compteur++;
-        }
-        id=malloc((compteur+1)*sizeof(char));
-        if(id==NULL){
-            printf("Allocation échouée\n");
-            exit(1);
-        }
-        fseek(fichier,-(compteur+1),SEEK_CUR); //curseur replacé devant le premier chiffre de l'id
-        compteur=0;
-        printf ("%s",id);
-	
-	
-}
-
-
-int main(){
-	lecture();
-	return 0;
+       
 }
