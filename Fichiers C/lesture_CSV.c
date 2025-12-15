85504ce3392fcfc4582c27c4d1283318a5a95825
aranney17 <aranney.sivakumar@etu.cyu.fr>
2025-12-15 23:52:41 +0100

    Refactor stockage function and rename lecture to lectureligne

--- Fichiers C/lesture_CSV.c
+++ Fichiers C/lesture_CSV.c
@@ -1,31 +1,34 @@
 #include "lecture_CSV.h"
 
 	
-	
+// procédure qui permet d'allouer dynamiquement l'espace nécessaire pour stocker l'information	
 void stockage(char* tmp, char caractere,FILE* fichier){
+
+	// variables locales
 	int compteur=0;
-		
-	// alloue dynamiquement l'espace nécessaire pour stocker l'id de l'usine
-        while(fgetc(fichier)!=caractere){ // compte le nb de chiffres dans l'id
-            compteur++;
-        }
+
+	// compte le nb de caractères dans l'information
+    while(fgetc(fichier)!=caractere){ 
+        compteur++;
+    }
+
+	//Allocation dynamique
+    tmp=malloc((compteur+1)*sizeof(char));
+    if(tmp==NULL){
+        printf("Allocation échouée\n");
+        exit(1);
+    }
         
-        tmp=malloc((compteur+1)*sizeof(char));
-        if(tmp==NULL){
-            printf("Allocation échouée\n");
-            exit(1);
-        }
-        
-        fseek(fichier,-(compteur+1),SEEK_CUR); //curseur replacé devant le premier chiffre de l'id
-        fgets(tmp,compteur+1,fichier);
-        printf("%s\n",tmp);
-	fseek(fichier,1,SEEK_CUR);
+    fseek(fichier,-(compteur+1),SEEK_CUR); //curseur replacé devant le premier caractère de l'information
+    fgets(tmp,compteur+1,fichier);
+    //printf("%s\n",tmp);
+	fseek(fichier,1,SEEK_CUR); //curseur replacé devant le premier caractère de la prochaine information
 }
 	
 	
 
 
-void lecture(){
+void lectureligne(){
 
 	//Gestion du fichier
 	FILE* fichier=NULL;
@@ -49,28 +52,44 @@ void lecture(){
        
 }
 
-
+//Fonction qui détecte le type de ligne
 Type_ligne detecter_type(char *col2, char* col3){
+	
+	//ligne non détectée
 	if(col2 == NULL || col3 == NULL){
 		return INCONNU;
     }
+
+	//ligne SOURCE->USINE (captage)
     if((strstr(col2, "Spring") || strstr(col2, Well) || strstr(col2, Resurgence)) && strstr(col3, "Plant")){
 		return SOURCE_USINE;
     }
+
+	//ligne USINE
     if((strstr(col2, "Plant") || strstr(col2, "Module")) & col3[0] == '-'){
         return USINE;
     }
+
+	//ligne USINE->STOCKAGE (stockage)
     if((strstr(col2, "Spring") || strstr(col2, "Module")) && strstr(col3, "Storage")){
         return USINE_STOCKAGE;
     }
+
+	//ligne STOCKAGE->JONCTION (distribution principale)
     if(strstr(col2, "Storage") && strstr(col3, "Junction")){
         return STOCKAGE_JONCTION;
     }
+
+	//ligne JONCTION->RACCORDEMENT (distribution secondaire)
     if(strstr(col2, "Junction") && strstr(col3, "Service")){
         return JONCTION_RACC;
     }
+
+	//ligne RACCORDEMENT->USAGER (branchement)
     if(strstr(col2, "Service") && strstr(col3, "Cust")){
         return RACC_USAGER;
     }
+
+	//ligne non détectée
     return INCONNU;
 }
