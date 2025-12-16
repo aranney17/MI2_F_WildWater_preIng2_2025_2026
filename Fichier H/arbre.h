a590966692b45564e179ee77caa45b65595b2017
aranney17 <aranney.sivakumar@etu.cyu.fr>
2025-12-16 13:00:38 +0100

    Define Arbre and Chainon structures in arbre.h
    Add structure definitions for 'Arbre' and 'Chainon' in arbre.h

--- Fichier H/arbre.h
+++ Fichier H/arbre.h
@@ -1,1 +1,16 @@
+// Bibliothèques
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
+typedef struct arbre{
+	char* id;
+	struct arbre* fils;
+}Arbre;
+
+typedef struct chainon{
+	Arbre* arbre;
+  float fuite;
+	struct chainon* suivant;
+} Chainon;
