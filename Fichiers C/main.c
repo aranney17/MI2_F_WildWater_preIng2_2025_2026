592eed544bacdf91f6ef500bab5f97075018e94b
aranney17 <aranney.sivakumar@etu.cyu.fr>
2025-12-16 11:57:35 +0100

    Implement file opening and line storage in main
    Add file handling and line storage functionality

--- Fichiers C/main.c
+++ Fichiers C/main.c
@@ -1,1 +1,14 @@
-
+int main(){
+	
+	//Gestion du fichier
+	FILE* fichier=NULL;
+	fichier=fopen("c-wildwater_v0(1).dat","r+");
+	if (fichier==NULL ){
+		printf("Ouverture du fichier impossible\n");
+		exit(1);
+	}
+	
+	Ligne ligne;
+	ligne=stockage_ligne(fichier);
+	return 0;
+}
