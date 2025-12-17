1c52dfd40c08aa648748390f535ddc40709d5e4f
syrine-chellat <syrine.chellat@gmail.com>
2025-12-17 22:27:20 +0100

    Refactor CSV line processing functions

--- Fichiers C/lesture_CSV.c
+++ Fichiers C/lesture_CSV.c
@@ -2,7 +2,7 @@
 
 	
 // procédure qui permet de stocker l'information	
-char* stockage_info(char* tmp, char caractere,FILE* fichier){
+/*char* stockage_info(char* tmp, char caractere,FILE* fichier){
 
 	// variables locales
 	int compteur=0;
@@ -23,52 +23,35 @@ char* stockage_info(char* tmp, char caractere,FILE* fichier){
     fgets(tmp,compteur+1,fichier);
 	fseek(fichier,1,SEEK_CUR); //curseur replacé devant le premier caractère de la prochaine information
 	return tmp;
-}
+}*/
 
 //Fonction qui détecte le type de ligne
 Type_ligne detecter_type(char *col2, char* col3){
-	
-	//ligne non détectée
-	if(col2 == NULL || col3 == NULL){
-		return INCONNU;
-    }
-
-	//ligne SOURCE->USINE (captage)
-    if((strstr(col2, "Spring") || strstr(col2, "Well") || strstr(col2, "Resurgence") || strstr(col2, "Source") || strstr(col2, "Fountain")) && (strstr(col3, "Plant") || strstr(col3, "Module") || strstr(col3, "Unit"))){
-		return SOURCE_USINE;
-    }
-
-	//ligne USINE
-    if((strstr(col2, "Plant") || strstr(col2, "Module") || strstr(col2, "Unit")) && col3[0] == '-'){
-        return USINE;
-    }
-
-	//ligne USINE->STOCKAGE (stockage)
-    if((strstr(col2, "Plant") || strstr(col2, "Module") || strstr(col2, "Unit")) && strstr(col3, "Storage")){
-        return USINE_STOCKAGE;
-    }
-
-	//ligne STOCKAGE->JONCTION (distribution principale)
-    if(strstr(col2, "Storage") && strstr(col3, "Junction")){
-        return STOCKAGE_JONCTION;
-    }
-
-	//ligne JONCTION->RACCORDEMENT (distribution secondaire)
-    if(strstr(col2, "Junction") && strstr(col3, "Service")){
-        return JONCTION_RACC;
-    }
-
-	//ligne RACCORDEMENT->USAGER (branchement)
-    if(strstr(col2, "Service") && strstr(col3, "Cust")){
-        return RACC_USAGER;
-    }
-
-	//ligne non détectée
-    return INCONNU;
-	
+        if(col2 == NULL || col3 == NULL){
+                return INCONNU;
+        }
+        if((strstr(col2, "Spring") || strstr(col2, "Well") || strstr(col2, "Resurgence")) && strstr(col3, "Plant")){
+                return SOURCE_USINE;
+        }
+        if((strstr(col2, "Plant") || strstr(col2, "Module")) && col3[0] == '-'){
+                return USINE;
+        }
+        if((strstr(col2, "Spring") || strstr(col2, "Module")) && strstr(col3, "Storage")){
+                return USINE_STOCKAGE;
+        }
+        if(strstr(col2, "Storage") && strstr(col3, "Junction")){
+                return STOCKAGE_JONCTION;
+        }
+        if(strstr(col2, "Junction") && strstr(col3, "Service")){
+                return JONCTION_RACC;
+        }
+        if(strstr(col2, "Service") && strstr(col3, "Cust")){
+                return RACC_USAGER;
+        }
+        return INCONNU;
 }
 
-Ligne stockage_ligne(FILE* fichier){
+/*Ligne stockage_ligne(FILE* fichier){
 	
 	// variables locales
 	Ligne ligne;
@@ -83,4 +66,56 @@ Ligne stockage_ligne(FILE* fichier){
 	//printf("%s %s %s %s %s %d",ligne.col1,ligne.col2,ligne.col3,ligne.volume,ligne.leak,ligne.type);
 	return ligne;
 	
+}*/
+
+Ligne stockage_ligne(char* buffer){
+	if(buffer == NULL){
+		exit(1);
+	}
+        Ligne ligne;
+        char* tmp;
+
+        tmp = strtok(buffer, ";");
+        if(tmp != NULL){
+                strcpy(ligne.col1, tmp);
+        }
+        else{
+                strcpy(ligne.col1, "");
+        }
+
+        tmp = strtok(NULL, ";");
+        if(tmp != NULL){
+                strcpy(ligne.col2, tmp);
+		printf("COL2 = [%s]\n", ligne.col2);
+        }
+        else{
+                strcpy(ligne.col2, "");
+		printf("COL2 = [%s]\n", ligne.col2);
+        }
+        tmp = strtok(NULL, ";");
+        if(tmp != NULL){
+                strcpy(ligne.col3, tmp);
+        }
+        else{
+                strcpy(ligne.col3, "");
+        }
+        tmp = strtok(NULL, ";");
+        if(tmp != NULL){
+                ligne.volume = atof(tmp);
+        }
+        else{
+                ligne.volume = 0;
+        }
+        tmp = strtok(NULL, ";");
+        if(tmp != NULL){
+                ligne.pertes = atof(tmp);
+        }
+        else{
+                ligne.pertes = 0;
+	 }
+
+        ligne.type = detecter_type(ligne.col2, ligne.col3);
+
+        return ligne;
 }
+
