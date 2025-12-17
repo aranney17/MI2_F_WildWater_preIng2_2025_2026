8ad5462e708738867b13758cddae611b580f16e6
aranney17 <aranney.sivakumar@etu.cyu.fr>
2025-12-17 01:06:43 +0100

    Refactor AVL tree functions for 'fuites' structure

--- Fichiers C/avl.c
+++ Fichiers C/avl.c
@@ -142,15 +142,125 @@ pArbre insertionAVL(pArbre a, char* identifiant, Usine* u, int* h){
 	return a;
 }
 
-AVL_fuites* creerAVL(char* identifiant, Usine u){
-	pArbre noeud = malloc(sizeof(Arbre));
+AVL_fuites* creerAVLfuites(char* identifiant, Arbre_fuite* element){
+	AVL_fuites* nv = malloc(sizeof(AVL_fuites));
 	if(noeud == NULL){
 		exit(1);
 	}
-	noeud->id = identifiant;
-	noeud->usine = u;
-	noeud->fg = NULL;
-	noeud->fd = NULL;
-	noeud->equilibre = 0;
-	return noeud;
+	nv->id = identifiant;
+	nv->elmt = element;
+	nv->fg = NULL;
+	nv->fd = NULL;
+	nv->equilibre = 0;
+	return nv;
+}
+
+int rechercheAVLfuites(Arbre_fuite* a, char* identifiant){
+	if(a == NULL){
+		return 0;
+	}
+	if(strcmp(a->id, identifiant) == 0){
+		return a;
+	}
+	if(strcmp(a->id, identifiant) > 0){
+		return rechercheAVLfuites(a->fg, identifiant);
+	}
+	return rechercheAVLfuites(a->fd, identifiant);
+}
+
+AVL_fuites* rotationDroiteAVLfuites(AVL_fuites* a){
+	AVL_fuites* pivot;
+	int eq_a;
+	int eq_p;
+	pivot = a->fg;
+	a->fg = pivot->fd;
+	pivot->fd = a;
+	eq_a = a->equilibre;
+	eq_p = pivot->equilibre;
+	a->equilibre = eq_a - min(eq_p,0)+1;
+	pivot->equilibre = max(eq_a+2,max(eq_a+eq_p+2,eq_p+1));
+	a = pivot;
+	return a;
+}
+
+AVL_fuites* rotationGaucheAVLfuites(AVL_fuites* a){
+	AVL_fuites* pivot;
+	int eq_a;
+	int eq_p;
+	pivot = a->fd;
+	a->fd = pivot->fg;
+	pivot->fg = a;
+	eq_a = a->equilibre;
+	eq_p = pivot->equilibre;
+	a->equilibre = eq_a - max(eq_p,0)-1;
+	pivot->equilibre = min(eq_a-2,min(eq_a+eq_p-2,eq_p-1));
+	a = pivot;
+	return a;
+}
+
+AVL_fuites* doubleRotationGaucheAVLfuites(AVL_fuites* a){
+	if(a == NULL){
+		exit(1);
+	}
+	a->fd = rotationDroiteAVLfuites(a->fd);
+	return rotationGaucheAVLfuites(a);
+}
+
+AVL_fuites* doubleRotationDroiteAVLfuites(AVL_fuites* a){
+	if(a == NULL){
+		exit(1);
+	}
+	a->fd = rotationGaucheAVLfuites(a->fg);
+	return rotationDroiteAVLfuites(a);
+}
+
+AVL_fuites* equilibreAVLfuites(AVL_fuites* a){
+	if(a == NULL){
+		exit(1);
+	}
+	if(a->equilibre >= 2){
+		if(a->fd->equilibre >= 0){
+			return rotationGaucheAVLfuites(a);
+		}
+		else{
+			return doubleRotationGaucheAVLfuites(a);
+		}
+	}
+	else if(a->equilibre <= -2){
+		if(a->fg->equilibre <= 0){
+			return rotationDroiteAVLfuites(a);
+		}
+		else{
+			return doubleRotationDroiteAVLfuites(a);
+		}
+	}
+}
+
+AVL_fuites* insertionAVLfuites(AVL_fuites* a, char* identifiant, Arbre_fuite* element, int* h){
+	if(a == NULL){
+		*h = 1;
+		return creerArbreAVLfuites(identifiant,element);
+	}
+	if(strcmp(a->id, identifiant) > 0){
+		a->fg = insertionAVLfuites(a->fg, identifiant, element, h);
+		*h = -*h;
+	}
+	else if(strcmp(identifiant, a->id) > 0){
+		a->fd = insertionAVLfuites(a->fd, identifiant, identifiant, h);
+	}
+	else{
+		*h = 0;
+		return a;
+	}
+	if(*h != 0){
+		a->equilibre += *h;
+		a = equilibreAVLfuites(a);
+		if(a->equilibre == 0){
+			*h = 0;
+		}
+		else{
+			*h = 1;
+		}
+	}
+	return a;
 }
