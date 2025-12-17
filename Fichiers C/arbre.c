0e28bfc3e1a9714d9304ac00d3f5dbfafc13397a
aranney17 <aranney.sivakumar@etu.cyu.fr>
2025-12-17 20:22:33 +0100

    Add function to create Arbre_fuite
    Implement function to create a new Arbre_fuite structure.

--- Fichiers C/arbre.c
+++ Fichiers C/arbre.c
@@ -1,1 +1,13 @@
 #include "arbre.h"
+
+Arbre_fuite* creerArbreFuite(char*identifiant){
+    Arbre_fuite* nv = malloc(sizeof(Arbre_fuite));
+    if(nv==NULL){
+        printf("Allocation échouée");
+        exit(1);
+    }
+    nv->id=strdup(identifiant);
+    nv->fils = NULL;
+    return nv;
+}
+
