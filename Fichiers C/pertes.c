e8624c935ff0e962b082f35bfbb98afcf88a5031
aranney17 <aranney.sivakumar@etu.cyu.fr>
2025-12-19 00:27:45 +0100

    Implement loss calculation and efficiency functions

--- Fichiers C/pertes.c
+++ Fichiers C/pertes.c
@@ -1,1 +1,36 @@
 #include "pertes.h"
+
+double calcul_pertes(Arbre_fuite* noeud, double volume){
+    
+    if(noeud->fils==NULL){
+        return 0.0;
+    }
+    
+    double pertes_totales=0.0;
+    double volume_par_fils;
+    double perte_troncon;
+    double volume_fils;
+    
+    Chainon* p=noeud->fils;
+    
+    while(p!=NULL){
+        volume_par_fils=volume/nbFils(noeud);
+        perte_troncon=volume_par_fils*p->fuite;
+        pertes_totales+=perte_troncon;
+        volume_fils=volume_par_fils*(1-p->fuite);
+        pertes_totales+=calcul_pertes(p->noeud, volume_fils);
+        p=p->suivant;
+    }
+    
+    return pertes_totales;
+}
+
+double rendement(AVL_fuites* avl, char* id_usine){
+    
+    AVL_fuites* avl_usine=rechercheAVLfuites(avl, id_usine);
+    if(avl_usine==NULL){
+        return -1.0;
+    }
+    double pertes=calcul_pertes(avl_usine->elmt,avl_usine->elmt->volume);
+    return pertes/pow(10,6);
+}
