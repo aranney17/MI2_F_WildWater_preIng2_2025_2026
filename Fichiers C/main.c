0a34536e7407aedee1fa8ff96ce452c00a3d9d40
syrine-chellat <syrine.chellat@gmail.com>
2025-12-18 18:16:39 +0100

    Refactor main function to remove file management
    Removed file handling and processing code.

--- Fichiers C/main.c
+++ Fichiers C/main.c
@@ -1,14 +1,6 @@
 int main(){
-	
-	//Gestion du fichier
-	FILE* fichier=NULL;
-	fichier=fopen("c-wildwater_v0(1).dat","r+");
-	if (fichier==NULL ){
-		printf("Ouverture du fichier impossible\n");
-		exit(1);
-	}
-	
-	Ligne ligne;
-	ligne=stockage_ligne(fichier);
+	pArbre a = NULL;
+	a = parcours(a);
+	generer_fichiers_histogramme(a);
 	return 0;
 }
