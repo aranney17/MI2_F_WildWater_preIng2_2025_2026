a9edf55154600b9dadf5a76fec6aaf1427aab99f
syrine-chellat <syrine.chellat@gmail.com>
2025-12-18 23:33:18 +0100

    Refactor main function to handle command-line arguments

--- Fichiers C/main.c
+++ Fichiers C/main.c
@@ -1,6 +1,48 @@
-int main(){
-	pArbre a = NULL;
-	a = parcours(a);
-	generer_fichiers_histogramme(a);
-	return 0;
+int main(int argc, char *argv[]){
+    if(argc < 3){
+        printf("Erreur : arguments insuffisants\n");
+        return 1;
+    }
+
+    char *fichier_donees = argv[1];
+    char *commande  = argv[2];
+
+    if(strcmp(commande, "histo") == 0){
+        if(argc != 4){
+            printf("Erreur : histo nécessite max|src|real\n");
+            return 1;
+        }
+
+        char *mode = argv[3];
+
+        if(strcmp(mode, "max") != 0 &&
+        	strcmp(mode, "src") != 0 &&
+        	strcmp(mode, "real") != 0){
+			printf("Erreur : mode histo invalide\n");
+            return 1;
+        }
+
+		pArbre a = NULL;
+		a = parcours(a);
+		generer_histogramme(fichier_donnees, mode);
+
+        return 0;
+    }
+    else if(strcmp(commande, "leaks") == 0){
+        if(argc != 4){
+            printf("Erreur : leaks nécessite un identifiant\n");
+            return 1;
+        }
+
+        char *usine_id = argv[3];
+
+        // Code leaks 
+
+        return 0;
+    }
+    else{
+        printf("Erreur : commande inconnue\n");
+        return 1;
+    }
 }
+
