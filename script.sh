cb0e75a8ee39a96c903e27c0a5557e8e921410d5
syrine-chellat <syrine.chellat@gmail.com>
2025-12-18 22:48:52 +0100

    Change CSV file extensions from .csv to .dat

--- script.sh
+++ script.sh
@@ -10,13 +10,13 @@ fi
 # Choix du fichier
 case "$1" in
 	max)
-	  CSV="vol_max.csv"
+	  CSV="vol_max.dat"
 	  ;;
   src)
-      CSV="vol_captation.csv"
+      CSV="vol_captation.dat"
       ;;
   reel)
-      CSV="vol_traitement.csv"
+      CSV="vol_traitement.dat"
       ;;
   *)
       echo "Argument invalide : $1"
@@ -26,11 +26,11 @@ case "$1" in
 esac
 
 # Tri par volume
-sort -t';' -k2,2n "$CSV" > vol_trie.csv
+sort -t';' -k2,2n "$CSV" > vol_trie.dat
 
 # Extraction
-head -n 51 vol_trie.csv > petites.csv
-tail -n 10 vol_trie.csv > grandes.csv
+head -n 51 vol_trie.csv > petites.dat
+tail -n 10 vol_trie.csv > grandes.dat
 
 # Gnuplot
 gnuplot -e "infile='petites.csv'; outfile='50_petites_${1}.png'" histo.gp
