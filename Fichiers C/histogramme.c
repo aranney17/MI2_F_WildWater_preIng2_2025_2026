31b56ac6ba97ae476f8ef8ad1223694d875b2d8d
syrine-chellat <syrine.chellat@gmail.com>
2025-12-19 15:38:15 +0100

    Update include path for histogramme.h

--- Fichiers C/histogramme.c
+++ Fichiers C/histogramme.c
@@ -1,4 +1,4 @@
-#include "histogramme.h"
+#include "fichiers_h/histogramme.h"
 
 //lecture_CSV pour chaque ligne on recherche dans l'avl on ajoute le volume a la bonne usine
 //ecrire vol_max vol_captation vol_traite
