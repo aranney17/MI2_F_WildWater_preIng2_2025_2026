9093cfee61829a7fb74a44a0784c367a5eff05b4
syrine-chellat <syrine.chellat@gmail.com>
2025-12-19 19:14:56 +0100

    Update pertes.h

--- Fichier H/pertes.h
+++ Fichier H/pertes.h
@@ -1,9 +1,17 @@
+#ifndef PERTES_H
+#define PERTES_H
+
+
+
 // Bibliothèques
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include "arbre.h"
 
 double calcul_pertes(Arbre_fuite* noeud, double volume);
 double rendement(AVL_fuites* avl, char* id_usine);
 void Historique(char* id_usine,float pertes);
+
+#endif
