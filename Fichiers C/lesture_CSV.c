38e1fdf680323c489e02c8a3ba5c1027a337ef71
aranney17 <aranney.sivakumar@etu.cyu.fr>
2025-12-19 01:54:14 +0100

    Enhance type detection logic in detecter_type function

--- Fichiers C/lesture_CSV.c
+++ Fichiers C/lesture_CSV.c
@@ -30,13 +30,13 @@ Type_ligne detecter_type(char *col2, char* col3){
         if(col2 == NULL || col3 == NULL){
                 return INCONNU;
         }
-        if((strstr(col2, "Spring") || strstr(col2, "Well") || strstr(col2, "Resurgence")) && strstr(col3, "Plant")){
+        if((strstr(col2, "Spring") || strstr(col2, "Well") || strstr(col2, "Resurgence")) && (strstr(col3, "Plant") || strstr(col3, "Module") || strstr(col3, "Unit"))){
                 return SOURCE_USINE;
         }
         if((strstr(col2, "Plant") || strstr(col2, "Module")) && col3[0] == '-'){
                 return USINE;
         }
-        if((strstr(col2, "Spring") || strstr(col2, "Module")) && strstr(col3, "Storage")){
+        if((strstr(col2, "Plant") || strstr(col2, "Module") || strstr(col2, "Unit")) && strstr(col3, "Storage")){
                 return USINE_STOCKAGE;
         }
         if(strstr(col2, "Storage") && strstr(col3, "Junction")){
