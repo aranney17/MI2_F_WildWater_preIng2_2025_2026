75288034c413e9f028e8a332f9bb9c7e42c81737
syrine-chellat <syrine.chellat@gmail.com>
2025-12-19 15:27:55 +0100

    Add function declarations to histogramme.h

--- Fichier H/histogramme.h
+++ Fichier H/histogramme.h
@@ -1,1 +1,12 @@
+// Bibliothèques
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
+pArbre parcours(pArbre a, const char* fichier_donnees);
+void remplir_infixe(pArbre a, const char* mode);
+void generer_fichier_max(pArbre a, const char* mode);
+void generer_fichier_src(pArbre a, const char* mode);
+void generer_fichier_real(pArbre a, const char* mode);
+void traitement_histogramme(const char* fichier_donnees, const char* mode);
