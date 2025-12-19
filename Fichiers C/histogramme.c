aaecf78fda6f602804755e381fa2e3b22851d30a
syrine-chellat <syrine.chellat@gmail.com>
2025-12-19 14:14:52 +0100

    Refactor histogram functions for better clarity

--- Fichiers C/histogramme.c
+++ Fichiers C/histogramme.c
@@ -3,9 +3,9 @@
 //lecture_CSV pour chaque ligne on recherche dans l'avl on ajoute le volume a la bonne usine
 //ecrire vol_max vol_captation vol_traite
 
-pArbre parcours(pArbre a){
+pArbre parcours(pArbre a, const char* fichier_donnees){
     char buffer[255];
-    FILE* fichier = fopen("c-wildwater_v0.dat","r");
+    FILE* fichier = fopen(fichier_donnees,"r");
     if(fichier == NULL){
         exit(1);
     }
@@ -39,38 +39,92 @@ pArbre parcours(pArbre a){
     return a;
 }
 
-void remplir_fichiers(pArbre a, FILE *fmax, FILE *fcap, FILE *freal){
+void remplir_infixe(pArbre a, const char* mode){
+    if(a == NULL){
+        return;
+    }
+
+    remplir_infixe(a->fg, mode);
+    
+    if(strcmp(mode, "max") == 0){
+        fprintf(fmax,  "%s;%lf\n", a->id, a->usine->vol_max);
+    }
+    else if(strcmp(mode, "src") == 0){
+        fprintf(fmax,  "%s;%lf\n", a->id, a->usine->vol_capte);
+    } else if(strcmp(mode, "real") == 0){
+        fprintf(fmax,  "%s;%lf\n", a->id, a->usine->vol_reel);
+    }
+
+    remplir_infixe(a->fd, mode);
+    
+}
+
+/*void remplir_fichier(pArbre a, FILE *fmax, FILE *fcap, FILE *freal){
     if(a == NULL){
          return;
     }
 
     remplir_fichiers(a->fg, fmax, fcap, freal);
 
-    fprintf(fmax,  "%s;%.2f\n", a->id, a->usine->vol_max);
-    fprintf(fcap,  "%s;%.2f\n", a->id, a->usine->vol_capte);
-    fprintf(freal, "%s;%.2f\n", a->id, a->usine->vol_reel);
+    fprintf(fmax,  "%s;%lf\n", a->id, a->usine->vol_max);
+    fprintf(fcap,  "%s;%lf\n", a->id, a->usine->vol_capte);
+    fprintf(freal, "%s;%lf\n", a->id, a->usine->vol_reel);
 
     remplir_fichiers(a->fd, fmax, fcap, freal);
-}
+}*/
 
-void generer_fichiers_histogramme(char* fichier, char* mode, pArbre a){
+void generer_fichier_max(pArbre a, const char* mode){
     FILE *fmax  = fopen("vol_max.dat", "w");
-    FILE *fcap  = fopen("vol_captation.dat", "w");
-    FILE *freal = fopen("vol_traitement.dat", "w");
-
-    if(fmax == NULL || fcap == NULL || freal == NULL){
-        printf("erreur ouverture fichiers histogramme");
+    if(fmax == NULL){
+        printf("erreur ouverture fichier histogramme");
         exit(1);
     }
 
-    fprintf(fmax,  "id;volume_max\n");
-    fprintf(fcap,  "id;volume_captation\n");
-    fprintf(freal, "id;volume_traitement\n");
+    fprintf(fmax,  "identifiant;volume max (k.m3.year-1)\n");
 
-    remplir_fichiers(a, fmax, fcap, freal);
+    remplir_infixe(a, mode);
 
     fclose(fmax);
+}
+
+void generer_fichier_src(pArbre a, const char* mode){
+    FILE *fcap  = fopen("vol_captation.dat", "w");
+    if(fcap == NULL ){
+        printf("erreur ouverture fichier histogramme");
+        exit(1);
+    }
+
+    fprintf(fcap,  "identifiant;volume capté (k.m3.year-1)\n");
+
+    remplir_infixe(a, mode);
+
     fclose(fcap);
+}
+
+void generer_fichier_max(pArbre a, const char* mode){
+    FILE *freal = fopen("vol_traitement.dat", "w");
+    if(freal == NULL){
+        printf("erreur ouverture fichier histogramme");
+        exit(1);
+    }
+
+    fprintf(freal, "identifiant;volume traité (k.m3.year-1)\n");
+
+    remplir_infixe(a, mode);
+
     fclose(freal);
 }
 
+void traitement_histogramme(const char* fichier_donnees, const char* mode){
+    pArbre a = NULL;
+    a = parcours(a, fichier_donnees);
+    if(strcmp(mode, max) == 0){
+         generer_fichier_max(a, mode);
+    }
+    if(strcmp(mode, max) == 0){
+         generer_fichier_max(a, mode);
+    }
+    if(strcmp(mode, max) == 0){
+         generer_fichier_max(a, mode);
+    }
+}
