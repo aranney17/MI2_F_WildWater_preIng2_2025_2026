f17cec5b1586baecfd86c7f2baa4652e0cf24164
syrine-chellat <syrine.chellat@gmail.com>
2025-12-19 15:33:53 +0100

    Add function declarations to pertes.h

--- Fichier H/pertes.h
+++ Fichier H/pertes.h
@@ -1,1 +1,9 @@
+// Bibliothèques
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
+double calcul_pertes(Arbre_fuite* noeud, double volume);
+double rendement(AVL_fuites* avl, char* id_usine);
+void Historique(char* id_usine,float pertes);
