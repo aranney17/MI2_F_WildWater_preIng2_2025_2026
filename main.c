ad5ca05c1f091b8a8fcf3922578db61ac21d3ae0
syrine-chellat <syrine.chellat@gmail.com>
2025-12-21 21:22:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]){
     char *fichier_donees = argv[1];
     char *commande  = argv[2];
 
-    if(strcmp(commande, "histo") == 0){
+    if(strcmp(commande, "histo") == 0){     //histo max|src|real -> générer histogramme
         if(argc != 4){
             printf("Erreur : histo nécessite max|src|real\n");
             return 1;
@@ -28,12 +28,11 @@ int main(int argc, char *argv[]){
 			printf("Erreur : mode histo invalide\n");
             return 1;
         }
-		printf(" ICIIIIIIII %s \n", fichier_donees);
-		traitement_histogramme(fichier_donees, mode);
+		traitement_histogramme(fichier_donees, mode);   //générer l'histogramme demandé
 
         return 0;
     }
-    else if(strcmp(commande, "leaks") == 0){
+    else if(strcmp(commande, "leaks") == 0){      //leaks "Usine" -> calcul pertes
         if(argc != 4){
             printf("Erreur : leaks nécessite un identifiant\n");
             return 1;
@@ -41,8 +40,8 @@ int main(int argc, char *argv[]){
 
         char *usine_id = argv[3];
 
-        AVL_fuites* avl=remplissage_arbre(fichier_donees);
-    	Historique(usine_id,rendement(avl, usine_id)); 
+        AVL_fuites* avl = remplissage_arbre(fichier_donees);
+    	Historique(usine_id, rendement(avl, usine_id));      //création fichier rendements.dat
 
         return 0;
     }
